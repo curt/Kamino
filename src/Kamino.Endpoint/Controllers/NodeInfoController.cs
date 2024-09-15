6003fc2cca1c44b4ba2723dcd7926f58d71e4d88
Curt Gilman <14070+curt@users.noreply.github.com>
2024-09-15 07:18:00 -0700

    feat: introduce minimal nodeinfo

---
+++ src/Kamino.Endpoint/Controllers/NodeInfoController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kamino.Endpoint.Controllers;
+
+[Route("")]
+[ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any, NoStore = false)]
+public class NodeInfoController : Controller
+{
+    [HttpGet(".well-known/nodeinfo")]
+    public ActionResult Index()
+    {
+        var model = new Dictionary<string, object>
+        (
+            [
+                new
+                (
+                    "links", new Dictionary<string, object?>
+                    (
+                        [
+                            new("rel", "http://nodeinfo.diaspora.software/ns/schema/2.0"),
+                            new("href", Url.ActionLink( action: "Get", values: new { Version = "2.0" }))
+                        ]
+                    )
+                )
+            ]
+        );
+
+        return Json(model);
+    }
+
+    [HttpGet("nodeinfo/{version}")]
+    public ActionResult Get(string version)
+    {
+        if (version != "2.0")
+        {
+            return NotFound();
+        }
+
+        var model = new Dictionary<string, object>
+        (
+            [
+                new("version", "2.0"),
+                new("protocols", new List<string>(["activitypub"])),
+                new
+                (
+                    "software",
+                    new Dictionary<string, string>
+                    (
+                        [
+                            new("name", "Klaxon"),
+                            new("version", ThisAssembly.AssemblyInformationalVersion)
+                        ]
+                    )
+                ),
+                new
+                (
+                    "services",
+                    new Dictionary<string, List<string>>([new("outbound", []), new("inbound", [])])
+                )
+            ]
+        );
+
+        return Json(model);
+    }
+}
+
+/*
+
+    "protocols": [
+        "activitypub"
+    ],
+    "services": {
+        "outbound": [],
+        "inbound": []
+    },
+*/
