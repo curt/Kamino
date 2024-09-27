f481643bc38cccf61b4f90566143903330bcd214
Curt Gilman <14070+curt@users.noreply.github.com>
2024-09-27 15:54:42 -0700

    fix: set correct content type on activity+json response

--- src/Kamino.Endpoint/Controllers/ContextualController.cs
+++ src/Kamino.Endpoint/Controllers/ContextualController.cs
@@ -6,27 +6,34 @@ namespace Kamino.Endpoint.Controllers;
 
 public abstract class ContextualController : Controller
 {
-    private static readonly string[] ActivityMediaTypes = [
+    private static readonly string[] ActivityMediaTypes =
+    [
         "application/ld+json",
         "application/activity+json",
-        "application/json"
+        "application/json",
     ];
 
-    protected async Task<IActionResult> Contextualize(Func<Task<IActionResult>> html, Func<Task<IActionResult>> json)
+    protected async Task<IActionResult> Contextualize(
+        Func<Task<IActionResult>> html,
+        Func<Task<IActionResult>> json
+    )
     {
         var acceptHeaders = Request.GetTypedHeaders().Accept;
-        var jsonAcceptHeader = acceptHeaders.FirstOrDefault(x => ActivityMediaTypes.Contains(x.MediaType.Value));
+        var jsonAcceptHeader = acceptHeaders.FirstOrDefault(x =>
+            ActivityMediaTypes.Contains(x.MediaType.Value)
+        );
 
         if (jsonAcceptHeader != null)
         {
-            Response.ContentType = jsonAcceptHeader.MediaType.Buffer;
+            Response.ContentType = "application/activity+json";
             return await json();
         }
 
         return await html();
     }
 
-    protected TModel Contextify<TModel>(TModel core) where TModel : ActivityModelBase
+    protected TModel Contextify<TModel>(TModel core)
+        where TModel : ActivityModelBase
     {
         core.JsonLdContext = "https://www.w3.org/ns/activitystreams";
 
