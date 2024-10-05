6a6651a2b280608a506ca2e4a9a65efcc0ecfcfe
Curt Gilman <14070+curt@users.noreply.github.com>
2024-10-05 07:38:59 -0700

    fix: send correct content type for profile

--- src/Kamino.Public/Controllers/ProfilesController.cs
+++ src/Kamino.Public/Controllers/ProfilesController.cs
@@ -15,25 +15,16 @@ public class ProfilesController(ProfilesService profilesService) : ContextualCon
     }
 
     [HttpGet("index.html")]
-    [Produces("text/html")]
     public async Task<IActionResult> IndexHtml()
     {
         return await Task.FromResult(View("index.html"));
     }
 
     [HttpGet("index.json")]
-    [Produces("application/json", "application/activity+json", "application/ld+json")]
     public async Task<IActionResult> IndexJson()
     {
-        var model = await IndexModel();
-
-        return Json(Contextify(model));
-    }
-
-    private async Task<ProfileActivityModel> IndexModel()
-    {
         var model = await profilesService.GetPublicProfileAsync();
-
-        return model;
+        Response.ContentType = "application/activity+json; charset=utf-8";
+        return Json(Contextify(model));
     }
 }
